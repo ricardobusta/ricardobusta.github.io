69dbf5c93a9442a318da11e5e880a23bc3d0bf6b
Ricardo Bustamante <ricardo@busta.dev>
2024-02-11 16:34:29 +0100

    update image generation script

--- generateAssets.js
+++ generateAssets.js
@@ -9,23 +9,62 @@ const context = canvas.getContext('2d');
 const projectsAssetPath = './_srcassets/projects/';
 const outputPath = './assets/projects/';
 
-const files = fs.readdirSync(projectsAssetPath);
+function getChildren(path, isDirectory) {
+    return fs.readdirSync(path).filter(function(file){
+        return fs.statSync(`${path}/${file}`).isDirectory() === isDirectory;
+    })
+}
 
-files.forEach(file => {
-    console.log(file);
-    can.loadImage(projectsAssetPath + file).then(image => {
-        const maxSide = Math.max(image.width, image.height);
-        const scale = size / maxSide;
-        const w = image.width * scale;
-        const h = image.height * scale;
-        const x = (size - w) / 2;
-        const y = (size - h) / 2;
+function ensureDirSync (dirpath) {
+    try {
+        return fs.mkdirSync(dirpath)
+    } catch (err) {
+        if (err.code !== 'EEXIST') throw err
+    }
+}
 
-        context.fillStyle = '#000000ff';
-        context.fillRect(0, 0, size, size);
-        context.drawImage(image, x, y, w, h);
-        const buffer = canvas.toBuffer(file.endsWith('png') ? 'image/png' : 'image/jpeg');
-        fs.writeFileSync(outputPath + "/" + file, buffer);
+function resizeImage(image){
+    const maxSide = Math.max(image.width, image.height);
+    const scale = size / maxSide;
+    const w = image.width * scale;
+    const h = image.height * scale;
+    const x = (size - w) / 2;
+    const y = (size - h) / 2;
+
+    context.fillStyle = '#000000ff';
+    context.fillRect(0, 0, size, size);
+    context.drawImage(image, x, y, w, h);
+
+    return canvas.toBuffer('image/png');
+}
+
+const dirs = getChildren(projectsAssetPath, true);
+
+ensureDirSync(outputPath);
+
+dirs.forEach(dir => {
+    const year = dir;
+    const files = getChildren(projectsAssetPath+dir, false);
+    files.forEach(file => {
+        try {
+            can.loadImage(projectsAssetPath + year + "/" + file).then(image => {
+                const buffer = resizeImage(image)
+                if (file.endsWith("jpg")) {
+                    file = file.replace("jpg", "png");
+                }
+                if (file.endsWith("-img.png")) {
+                    file = file.replace("-img.png", "-img0.png");
+                }
+                if (file.endsWith("-ss.png")) {
+                    file = file.replace("-ss.png", "-img1.png");
+                }
+                const path = outputPath + year + "-" + file;
+                console.log(`success: ${path}`)
+                fs.writeFileSync(path, buffer);
+            });
+        }catch(e){
+            console.log(`failed: ${file}`)
+        }
     });
 });
 
